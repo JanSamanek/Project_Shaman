1e27da5069cec10fd161b650a85e9078b2f7f089
JanSamanek <jan.d.samanek@gmail.com>
2023-01-20 19:27:30 +0100

    good job!

---
+++ Siamese Network/utils/plot_training.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+def plot_training(H, plotPath):
+	# construct a plot that plots and saves the training history
+	plt.style.use("ggplot")
+	plt.figure()
+	plt.plot(H.history["loss"], label="train_loss")
+	plt.plot(H.history["val_loss"], label="val_loss")
+	plt.plot(H.history["accuracy"], label="train_acc")
+	plt.plot(H.history["val_accuracy"], label="val_acc")
+	plt.title("Training Loss and Accuracy")
+	plt.xlabel("Epoch #")
+	plt.ylabel("Loss/Accuracy")
+	plt.legend(loc="lower left")
+	plt.savefig(plotPath)
