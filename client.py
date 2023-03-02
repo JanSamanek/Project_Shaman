de00f7ea422f3c6a021d893dbe8fb5820b2863b1
JanSamanek <jan.d.samanek@gmail.com>
2023-03-02 13:01:10 +0100

    moquito rewrite not tested

--- client.py
+++ client.py
@@ -1,94 +1,61 @@
 import socket
-import cv2
 import subprocess
 import json
 import time
-#from jetbot import Robot
-
-class Client:
-    def __init__(self, host):   
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
-        self.host = host
-        
-    def connect_to_server(self, server_port=8080):
-        # Connect to the server
-        print(f"[INF] Trying to connect to ip adress: {self.host}, port: {server_port} ...")
-        self.client_socket.connect((self.host, server_port))
-        print(f"[INF] Connected to ip adress: {self.host}, port: {server_port} ...")
-
-    def start_streaming(self, gstreamer_port=5000):
-        print(f"[INF] Deploying Gstreamer pipeline ...")
-        pipeline = f"gst-launch-1.0 nvarguscamerasrc ! 'video/x-raw(memory:NVMM),width=1280, height=720, framerate=30/1, format=NV12' ! nvvidconv ! jpegenc ! rtpjpegpay ! udpsink host={self.host} port={gstreamer_port}"
-        self.gstreamer_pipeline = subprocess.Popen(pipeline, stdout=subprocess.PIPE, shell=True)
-        print(f"[INF] Streaming video to ip adress: {self.host}, port: {gstreamer_port} ...")
-
-    def communicate(self):
-        robot = Robot()
-
-        while True:
-            time_start = time.time()
-            json_data = self._recieve_json()
-
-            mot_speed_1, mot_speed_2 = json_data['mot_speed'] 
-            stop = json_data['stop']
-
-            if stop:
-                robot.stop()
-                print("[INF] Stopping robot and disconnecting from server ...")
-                self.disconnect()
-                break
-            elif mot_speed_1 is not None and mot_speed_2 is not None:
-                robot.set_motors(mot_speed_1, mot_speed_2)
-            elif mot_speed_1 is None or mot_speed_2 is None:
-                robot.stop()            
-            
-            # testing socket speed
-            time_end = time.time()
-            elapsed_time = time_end - time_start
-            print("Time to send and recieve instructions: ", elapsed_time)
-            
-            
-    def _send_img(self, img):
-        result, image = cv2.imencode('.jpg', img)                           # Convert the frame to a JPEG image
-        data = image.tobytes()                                              # Convert the image to a byte array
-        self.client_socket.sendall(len(data).to_bytes(4, byteorder='big'))  # Send the image size
-        self.client_socket.sendall(data)                                    # Send the image data
-    
-    def _send_ack(self):
-        bit = 1
-        self.client_socket.sendall(bit.to_bytes(1, byteorder='big'))
-
-    def _recieve_json(self):
-        size = int.from_bytes(self.client_socket.recv(4), byteorder='big')
-        json_data = self.client_socket.recv(size)
-        return json.loads(json_data)
-
-    def disconnect(self):
-        self.gstreamer_pipeline.terminate()
-        print("[INF] Gstreamer pipeline disconnected ...")
-        self.client_socket.close()
-        print("[INF] Connection closed ...")
+from jetbot import Robot
 
 import paho.mqtt.client as mqtt
 
 class Subscriber():
     def __init__(self, address, topic="jetbot_instructions", port=8080):
+        self.address = address
+        self.robot = Robot()
         self.client = mqtt.Client()
-        self.client.connect(address, port)
+        self.connect_to_broker(address, port)
         self.client.subscribe(topic)
-        self.client.on_message = self.on_message
+        self.client.on_message = self.control_robot
 
-    def on_message(self, client, userdata, message):
-        #json_data = json.loads(message.payload.decode())
-        print(message.payload.decode())
+    def start_gstreamer(self, gstreamer_port=5000):
+        print(f"[INF] Deploying Gstreamer pipeline ...")
+        pipeline = f"gst-launch-1.0 nvarguscamerasrc ! 'video/x-raw(memory:NVMM),width=1280, height=720, framerate=30/1, format=NV12' ! nvvidconv ! jpegenc ! rtpjpegpay ! udpsink host={self.address} port={gstreamer_port}"
+        self.gstreamer_pipeline = subprocess.Popen(pipeline, stdout=subprocess.PIPE, shell=True)
+        print(f"[INF] Streaming video to ip adress: {self.address}, port: {gstreamer_port} ...")
+
+    def connect_to_broker(self, address, port):
+        self.client.connect(address, port)
+        print(f"[INF] Subscriber connected to broker on address: {address}, port: {port} ...")
+
+    def control_robot(self, client, userdata, message):
+        time_start = time.time()
+        json_data = json.loads(message.payload.decode())
+
+        mot_speed_1, mot_speed_2 = json_data['mot_speed'] 
+        stop = json_data['stop']
+
+        if stop:
+            self.robot.stop()
+            print("[INF] Stopping robot and disconnecting from server ...")
+            self.stop()
+        elif mot_speed_1 is not None and mot_speed_2 is not None:
+            self.robot.set_motors(mot_speed_1, mot_speed_2)
+        elif mot_speed_1 is None or mot_speed_2 is None:
+            self.robot.stop()            
+            
+            # testing socket speed
+            time_end = time.time()
+            elapsed_time = time_end - time_start
+            print("Time to send and recieve instructions: ", elapsed_time)
 
     def run(self):
         self.client.loop_forever()
     
     def stop(self):
+        self.gstreamer_pipeline.terminate()
+        print("[INF] Gstreamer pipeline disconnected ...")
         self.client.disconnect()
-
-
+        print("[INF] Subscriber disconnected from broker ...")
+        
+        
 if __name__ == '__main__':
     import argparse
 
@@ -98,9 +65,3 @@ if __name__ == '__main__':
     
     subscriber = Subscriber(args.ip)
     subscriber.run()
-
-    # client = Client(args.ip)
-    # client.start_streaming()
-    # client.connect_to_server()
-    # client.communicate()
-    
--- server.py
+++ server.py
@@ -1,31 +1,31 @@
-import socket
 import cv2
-import numpy as np
 import json
 from tracker import create_tracker
 from Utilities.display_functions import display_fps, display_motor_speed
+import paho.mqtt.client as mqtt
+import subprocess
 
-class Server():
-    def __init__(self, server_port=8080, gstreamer_port=5000):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # Create a socket object
-        self.server_port = server_port
-        self.gstreamer_port = gstreamer_port
-        self._start_server()
-        
-    def _start_server(self):
-        self.server_socket.bind(('', self.server_port))
-        print(f"[INF] Server listening on port: {self.server_port}...")
-        self.server_socket.listen(5)
     
-    def accept_new_client(self):
-        self.client_socket, address = self.server_socket.accept()       # Wait for a client to connect
-        print(f"[INF] Client connected from ip adress: {address[0]}...")
+class Publisher():
+    def __init__(self, topic="jetbot_instructions", address="localhost", broker_port=8080, gstreamer_port=5000):
+        self.topic = topic
+        self.client = mqtt.Client()
+        self.gstreamer_port=gstreamer_port
+        self.broker = Publisher._start_broker(broker_port)
+        self._connect_to_broker(address, broker_port)
 
-    def communicate(self, save_video=False):
-        tracker = None
+    @staticmethod
+    def _start_broker(port):
+        print(f"[INF] Starting broker on port: {port} ...")
+        return subprocess.Popen(f'mosquitto -p {port}', shell=True)
+
+    def _connect_to_broker(self, address, port):
+        self.client.connect(address, port)
+        print(f"[INF] Publisher connected to broker on address: {address}, port: {port} ...")
+
+    def publish_data(self, save_video=False):
         turn_gain = 0.5
-        mot_speed_1, mot_speed_2 = None, None
-        offset = None
+        tracker, mot_speed_1, mot_speed_2, offset = None, None, None, None
         previous_time = 0
         
         pipeline = f"gst-launch-1.0 udpsrc port={self.gstreamer_port} ! application/x-rtp, encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink"
@@ -70,82 +70,26 @@ class Server():
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 json_data['stop'] = True
-                self._send_json(json_data)
+                self.client.publish(self.topic, json_data, qos=0)
                 break
             else:
                 json_data['stop'] = False
             
-            self._send_json(json_data)
+            json_data = json.dumps(json_data)
+            self.client.publish(self.topic, json_data, qos=0)
             
             cv2.imshow("*** TRACKING ***", img)
             cv2.waitKey(1)
         
         cap.release()
         print("[INF] Gtsreamer pipeline closed ... ")
-        self.close()
-            
-    def _send_json(self, json_data):
-        json_str = json.dumps(json_data)
-        data_bytes = json_str.encode()
-        
-        self.client_socket.sendall(len(data_bytes).to_bytes(4, byteorder='big'))
-        self.client_socket.sendall(data_bytes)
-    
-    def _recieve_ack(self):
-        self.client_socket.recv(1)
-
-    def _recieve_img(self):
-        size = int.from_bytes(self.client_socket.recv(4), byteorder='big')
-        data = b''
-        while len(data) < size:
-            data += self.client_socket.recv(1024)
-
-        # Decode image
-        image = np.frombuffer(data, np.uint8)          
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)  
-        
-        return image
-
-    def close(self):
-        print("[INF] Started server terminiation process...")  
-        self.client_socket.close()
-        self.server_socket.close()
-        print("[INF] Server shut down...")  
-
-import paho.mqtt.client as mqtt
-import subprocess
-
-    
-class Publisher():
-    def __init__(self, topic="jetbot_instructions", address="localhost", port=8080):
-        self.topic = topic
-        self.client = mqtt.Client()
-        self.broker = self._start_broker(port)
-        self._connect_to_broker(address, port)
-
-    @staticmethod
-    def _start_broker(port):
-        print("started broker")
-        return subprocess.Popen(f'mosquitto -p {port}', shell=True)
-
-    def _connect_to_broker(self, address, port):
-        self.client.connect(address, port)
-
-    def publish_data(self):
-
-        while True:
-            # Define the data to send
-            data = "Hello Jetson Nano"
-
-            # Publish the data to the MQTT topic
-            self.client.publish(self.topic, data)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        self.terminate()
 
     def terminate(self):
-        self.broker.terminate()
         self.client.disconnect()
+        print("[INF] Disconected publisher from broker ...")
+        self.broker.terminate()
+        print("[INF] Terminated broker ...")
 
 if __name__ == '__main__':
     publisher = Publisher()
