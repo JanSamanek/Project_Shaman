9e48ed3859374ffcafffc79f5dd90f95558a543b
JanSamanek <jan.d.samanek@gmail.com>
2023-03-03 20:58:02 +0100

    bugfix

--- client.py
+++ client.py
@@ -27,11 +27,6 @@ class Subscriber():
         print(f"[INF] Subscriber connected to broker on address: {address}, port: {port} ...")
 
     def control_robot(self, client, userdata, message):
-        time_start = None if 'time_start' not in locals() else time_start
-        time_end = time.time()
-        if time_start is not None:
-            elapsed_time = time_end - time_start
-            print("Time to send and recieve instructions: ", elapsed_time)
 
         json_data = json.loads(message.payload.decode())
 
@@ -46,8 +41,6 @@ class Subscriber():
             self.robot.set_motors(mot_speed_1, mot_speed_2)
         elif mot_speed_1 is None or mot_speed_2 is None:
             self.robot.stop()            
-            
-        time_start = time.time()
 
     def run(self):
         self.client.loop_forever()
