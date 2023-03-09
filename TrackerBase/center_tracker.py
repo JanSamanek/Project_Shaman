9f1581f5df78b0bdb18d3996c3a9a2f88b53d62b
JanSamanek <jan.d.samanek@gmail.com>
2023-03-09 09:54:24 +0100

    bug was beacause i left out kf update

--- TrackerBase/center_tracker.py
+++ TrackerBase/center_tracker.py
@@ -86,7 +86,7 @@ class PersonTracker:
 
                 # assign box
                 to.box = center_box_dict[tuple(new_center)]
-                #to.centroid = to.apply_kf(new_center)
+                to.apply_kf(new_center)
                 to.centroid = new_center
                 to.disappeared_count = 0
 
