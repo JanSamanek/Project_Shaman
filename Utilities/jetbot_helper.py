e913bb253cb6c7908e08a6f9bc3ab93f4f7e70de
JanSamanek <jan.d.samanek@gmail.com>
2023-03-10 17:44:45 +0100

    p adjustments

--- Utilities/jetbot_helper.py
+++ Utilities/jetbot_helper.py
@@ -1,4 +1,4 @@
-def get_motor_speed(offset, saturation=0.12, turn_gain=0.4):
+def get_motor_speed(offset, saturation=0.1, turn_gain=0.3):
     if offset is not None:
         mot_speed_1, mot_speed_2 = (turn_gain * offset, -turn_gain * offset)
         if abs(mot_speed_1) >= saturation or abs(mot_speed_2) >= saturation:
